acfb937b9aa922596ed6b8b9fa27b2da19ee6076
Geoff Golder <gdoteof@gmail.com>
2015-03-05 19:48:43 -0500

    add key

--- lq/add_key.sh
+++ lq/add_key.sh
@@ -1,1 +1,14 @@
-docker 
+usage () {
+  echo "Add a valid key to the license queue"
+  echo "Usage:./add_key LICENSEKEY_UUID"
+  exit 1
+}
+
+[ $# -lt 1 ] && {
+  usage
+}
+
+KEY=$1
+
+docker  pull redis
+docker run --rm redis redis-cli RPUSH vmxlicenses $KEY
