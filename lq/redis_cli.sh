ea027ad401439c99fe6fae19467e1c38395b7baf
Geoff Golder <gdoteof@gmail.com>
2015-03-07 17:40:00 -0500

    usable redis wrapepr

--- lq/redis_cli.sh
+++ lq/redis_cli.sh
@@ -1,3 +1,3 @@
 REDIS_SERVER=$(etcdctl ls /services/redis | cut -d'/' -f4 | head -n 1)
 
-docker run --rm redis redis-cli -h $REDIS_SERVER "$@"
+docker run --rm -i -t redis redis-cli -h $REDIS_SERVER "$@"
